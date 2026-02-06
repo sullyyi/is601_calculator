a88df9e49d0b2a5a67986796fef452753a6dd0bf
sullyyi <sullyyildiz@gmail.com>
2026-02-06 18:39:15 -0500

    added: init.py and operations

---
+++ calculator/__init__.py
@@ -0,0 +1,5 @@
+from .operations import add, subtract, multiply, divide
+
+__all__ = ["add", "subtract", "multiply", "divide"]
+#cleaner imports, centralized public intercace, library users can import from calculator instead of calculator.operations
+
---
+++ calculator/operations.py
@@ -0,0 +1,18 @@
+# calculator/operations.py
+
+def add(a: float, b: float) -> float:
+    return a + b
+
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+
+def divide(a: float, b: float) -> float:
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return a / b
